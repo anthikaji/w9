ae97cf02525475bf06d56a895d362099198f85dc
Anthika Jirattananon <anthikajir@mathregit.group>
2020-07-21 16:12:12 +0700

    new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { LoginComponent } from './login/login.component';
+import { ProductListComponent } from './product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login' , component: LoginComponent},
+  { path: 'list' , component: ProductListComponent},
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
